4eaa485fef7f7524ae34cb2dfafd20c9f6936a6d
PabloTirBar <pablo.tirado@edu.uah.es>
2025-03-12 13:59:15 +0100

    documentación primera parte

--- parte1/ejecutar.sh
+++ parte1/ejecutar.sh
@@ -1,7 +1,7 @@
 python3 src/test_planiffiers.py \
     --planner 'planificadores/lpg-td' \
     --domain 'pddl/dominio-drones.pddl' \
-    --start 2 \
+    --start-size 2 \
     --max-size 100 \
     --timeout 60
 
